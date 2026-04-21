b13d85f14224e1f6351561589438ad30ada6353c
kranthikumar jangam <kj24abh@herts.ac.uk>
2026-04-21 10:48:48 +0100

    mainjava class added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LeisureCentre centre = new LeisureCentre();
-        centre.run();
+        LeisureCentre centre = new LeisureCentre;
     }
 }
